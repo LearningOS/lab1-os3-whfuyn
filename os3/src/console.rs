6b3542fbb77762b4459bc58eab9904c8628f7ccf
Zhishi <39162745+whfuyn@users.noreply.github.com>
2022-07-22 15:53:37 +0000

    Adapt from `whfuyn/rcore-os`

--- os3/src/console.rs
+++ os3/src/console.rs
@@ -1,31 +1,34 @@
+use core::fmt;
+// use crate::sys_write;
 use crate::sbi::console_putchar;
-use core::fmt::{self, Write};
 
 struct Stdout;
 
-impl Write for Stdout {
+impl fmt::Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        for c in s.chars() {
-            console_putchar(c as usize);
-        }
+        s.bytes().for_each(|c| console_putchar(c as usize));
         Ok(())
     }
 }
 
 pub fn print(args: fmt::Arguments) {
+    use fmt::Write;
     Stdout.write_fmt(args).unwrap();
 }
 
 #[macro_export]
 macro_rules! print {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+        $crate::console::print(::core::format_args!($fmt $(, $($arg)+)?))
     }
 }
 
 #[macro_export]
 macro_rules! println {
+    () => {
+        println!("")
+    };
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
+        $crate::console::print(::core::format_args_nl!($fmt $(, $($arg)+)?))
+    };
 }
---
+++ os3/src/trap.rs
@@ -0,0 +1,57 @@
+mod context;
+
+use crate::task::{
+    run_next_task, exit_and_run_next, set_next_trigger,
+};
+use crate::println;
+use crate::syscall::syscall;
+pub use context::TrapContext;
+use core::arch::global_asm;
+use riscv::register::{
+    scause::{self, Exception, Interrupt, Trap},
+    stval, stvec,
+};
+
+global_asm!(include_str!("trap/trap.S"));
+extern "C" {
+    fn __all_traps();
+    pub fn __restore(cx: usize) -> !;
+}
+
+pub fn init() {
+    unsafe {
+        stvec::write(__all_traps as usize, stvec::TrapMode::Direct);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+    let scause = scause::read();
+    let stval = stval::read();
+
+    match scause.cause() {
+        Trap::Interrupt(Interrupt::SupervisorTimer) => {
+            // println!("\nscheduling");
+            set_next_trigger();
+            run_next_task();
+        }
+        Trap::Exception(Exception::UserEnvCall) => {
+            cx.sepc += 4;
+            let id = cx.x[17];
+            let args = [cx.x[10], cx.x[11], cx.x[12]];
+            cx.x[10] = syscall(id, args) as usize;
+        }
+        Trap::Exception(Exception::StoreFault | Exception::StorePageFault) => {
+            println!("[kernel] PageFault in application, kernel killed it.");
+            exit_and_run_next();
+        }
+        Trap::Exception(Exception::IllegalInstruction) => {
+            println!("[kernel] IllegalInstruction in application, kernel killed it.");
+            exit_and_run_next();
+        }
+        unknown => {
+            panic!("Unsupported trap {:?}, stval = {:#x}!", unknown, stval);
+        }
+    }
+    cx
+}
